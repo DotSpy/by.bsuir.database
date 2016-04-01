84b9c825466136d7139238b0d8f80670c6533f3d
Vlad Kiva <knijaz@gmail.com>
2016-04-01 09:19:22 +0300

    records edit

--- src/entity/database/Record.java
+++ src/entity/database/Record.java
@@ -1,12 +1,34 @@
 package entity.database;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
-public class Record {
-    private String table;
-    private List<String> columnHeaders = new ArrayList<>();
-    private List<Field> fieldList = new ArrayList<>();
-    private int recKey;
+public class Record implements Serializable {
 
+    private List<String> charField;
+    private List<Integer> intField;
+
+    public List<String> getCharField() {
+        return charField;
+    }
+
+    public void setCharField(List<String> charField) {
+        this.charField = charField;
+    }
+
+    public List<Integer> getIntField() {
+        return intField;
+    }
+
+    public void setIntField(List<Integer> intField) {
+        this.intField = intField;
+    }
+
+    public void addCharField(String s) {
+        this.charField.add(s);
+    }
+
+    public void addIntField(Integer i) {
+        this.intField.add(i);
+    }
 }
